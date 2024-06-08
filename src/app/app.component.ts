de0514ef92e146855f9103c35938c2467acf8abc
Ali Ahmed <MrAliAhmedM@gmail.com>
2024-06-08 14:54:11 +0300

    remove testing code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,32 +24,31 @@ import { isPlatformBrowser, isPlatformServer } from '@angular/common';
   styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
-  platformId = inject(PLATFORM_ID);
-  transferState = inject(TransferState);
+  // platformId = inject(PLATFORM_ID);
+  // transferState = inject(TransferState);
 
-  browserTime?: string;
-  serverTime?: string;
+  // browserTime?: string;
+  // serverTime?: string;
 
   ngOnInit() {
-    const serverTimeStateKey = makeStateKey<string>('serverTime');
-
-    if (isPlatformBrowser(this.platformId)) {
-      this.setBrowserTime();
-      setInterval(() => {
-        this.setBrowserTime();
-      }, 1000);
-      this.serverTime = this.transferState.get(
-        serverTimeStateKey,
-        "I don't know, I wasn't generated on the server."
-      );
-    } else if (isPlatformServer(this.platformId)) {
-      this.serverTime = new Date().toLocaleTimeString('en-US');
-      this.transferState.set(serverTimeStateKey, this.serverTime);
-      console.log('I am being rendered on the server');
-    }
+    // const serverTimeStateKey = makeStateKey<string>('serverTime');
+    // if (isPlatformBrowser(this.platformId)) {
+    //   this.setBrowserTime();
+    //   setInterval(() => {
+    //     this.setBrowserTime();
+    //   }, 1000);
+    //   this.serverTime = this.transferState.get(
+    //     serverTimeStateKey,
+    //     "I don't know, I wasn't generated on the server."
+    //   );
+    // } else if (isPlatformServer(this.platformId)) {
+    //   this.serverTime = new Date().toLocaleTimeString('en-US');
+    //   this.transferState.set(serverTimeStateKey, this.serverTime);
+    //   console.log('I am being rendered on the server');
+    // }
   }
 
-  setBrowserTime() {
-    this.browserTime = new Date().toLocaleTimeString('en-US');
-  }
+  // setBrowserTime() {
+  //   this.browserTime = new Date().toLocaleTimeString('en-US');
+  // }
 }
